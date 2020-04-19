44cf593aa071f13400d1ccf929080258324ad88b
Yamilett Estrada-Reyes <yyestrada-re@cps.edu>
2020-04-18 22:17:11 -0500

    test 1!

---
+++ script.js
@@ -0,0 +1,8 @@
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 380) {
+    $('.navbar').css('background','black');
+  } else {
+    $('.navbar').css('background','transparent');
+    }
+});
+
