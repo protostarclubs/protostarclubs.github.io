23e7a938a093be1a8d796b34749f646dfa3f3d8a
Yamilett Estrada-Reyes <36552200+yyestrada-re@users.noreply.github.com>
2020-10-10 18:32:17 -0500

    new

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 $(window).scroll(function () {
-  if ($(window).scrollTop() >= 380) {
+  if ($(window).scrollTop() >= 300) {
     $('.navbar').css('background','black');
   } else {
     $('.navbar').css('background','transparent');
     }
 });
 
+function myFunction() {
+  var x = document.getElementById("topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
